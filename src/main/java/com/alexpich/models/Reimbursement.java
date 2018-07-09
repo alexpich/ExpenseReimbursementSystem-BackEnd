a75e2a052447b6bd124a323f98ba4e8bf7269cf5
alexpich <pich.alex01@gmail.com>
2018-07-09 11:29:45 -0400

    Update User and Reimbursement models

--- src/main/java/com/alexpich/models/Reimbursement.java
+++ src/main/java/com/alexpich/models/Reimbursement.java
@@ -1,9 +1,12 @@
 package com.alexpich.models;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Component;
 
@@ -13,5 +16,235 @@ import org.springframework.stereotype.Component;
 public class Reimbursement implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	@Column(name = "reimbursement_id")
+	@NotNull
+	private int reimbursementId;
+
+	@Column(name = "amount")
+	@NotNull
+	private double amount;
+
+	@Column(name = "time_submitted")
+	@NotNull
+	private Date timeSubmitted;
+
+	@Column(name = "time_resolved")
+	@NotNull
+	private Date timeResolved;
+
+	@Column(name = "description")
+	@NotNull
+	private String description;
+
+	@Column(name = "author")
+	@NotNull
+	private int author;
+
+	@Column(name = "resolver")
+	@NotNull
+	private int resolver;
+
+	@Column(name = "status_id")
+	@NotNull
+	private int statusId;
+
+	@Column(name = "type_id")
+	@NotNull
+	private int typeId;
+
+	@Column(name = "status")
+	@NotNull
+	private String status;
+
+	@Column(name = "type")
+	@NotNull
+	private String type;
+
+	public Reimbursement(@NotNull int reimbursementId, @NotNull double amount, @NotNull Date timeSubmitted,
+			@NotNull Date timeResolved, @NotNull String description, @NotNull int author, @NotNull int resolver,
+			@NotNull int statusId, @NotNull int typeId, @NotNull String status, @NotNull String type) {
+		super();
+		this.reimbursementId = reimbursementId;
+		this.amount = amount;
+		this.timeSubmitted = timeSubmitted;
+		this.timeResolved = timeResolved;
+		this.description = description;
+		this.author = author;
+		this.resolver = resolver;
+		this.statusId = statusId;
+		this.typeId = typeId;
+		this.status = status;
+		this.type = type;
+	}
+
+	public int getReimbursementId() {
+		return reimbursementId;
+	}
+
+	public void setReimbursementId(int reimbursementId) {
+		this.reimbursementId = reimbursementId;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getTimeSubmitted() {
+		return timeSubmitted;
+	}
+
+	public void setTimeSubmitted(Date timeSubmitted) {
+		this.timeSubmitted = timeSubmitted;
+	}
+
+	public Date getTimeResolved() {
+		return timeResolved;
+	}
+
+	public void setTimeResolved(Date timeResolved) {
+		this.timeResolved = timeResolved;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
+	}
+
+	public int getResolver() {
+		return resolver;
+	}
+
+	public void setResolver(int resolver) {
+		this.resolver = resolver;
+	}
+
+	public int getStatusId() {
+		return statusId;
+	}
+
+	public void setStatusId(int statusId) {
+		this.statusId = statusId;
+	}
+
+	public int getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(int typeId) {
+		this.typeId = typeId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + author;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + reimbursementId;
+		result = prime * result + resolver;
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + statusId;
+		result = prime * result + ((timeResolved == null) ? 0 : timeResolved.hashCode());
+		result = prime * result + ((timeSubmitted == null) ? 0 : timeSubmitted.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + typeId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (author != other.author)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (reimbursementId != other.reimbursementId)
+			return false;
+		if (resolver != other.resolver)
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (statusId != other.statusId)
+			return false;
+		if (timeResolved == null) {
+			if (other.timeResolved != null)
+				return false;
+		} else if (!timeResolved.equals(other.timeResolved))
+			return false;
+		if (timeSubmitted == null) {
+			if (other.timeSubmitted != null)
+				return false;
+		} else if (!timeSubmitted.equals(other.timeSubmitted))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (typeId != other.typeId)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Reimbursement [reimbursementId=" + reimbursementId + ", amount=" + amount + ", timeSubmitted="
+				+ timeSubmitted + ", timeResolved=" + timeResolved + ", description=" + description + ", author="
+				+ author + ", resolver=" + resolver + ", statusId=" + statusId + ", typeId=" + typeId + ", status="
+				+ status + ", type=" + type + "]";
+	}
 
 }
--- src/main/java/com/alexpich/models/User.java
+++ src/main/java/com/alexpich/models/User.java
@@ -1,6 +1,8 @@
 package com.alexpich.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -56,6 +58,8 @@ public class User implements Serializable {
 	@NotNull
 	private String roleType;
 
+	private List<Reimbursement> reimbursements = new ArrayList<Reimbursement>();
+
 	public User(@NotNull String username, @NotNull String password, @NotNull String firstName, @NotNull String lastName,
 			@NotNull String email, @NotNull @Min(1) @Max(2) int roleId, @NotNull String roleType) {
 		super();
@@ -80,6 +84,153 @@ public class User implements Serializable {
 		this.email = email;
 		this.roleId = roleId;
 		this.roleType = roleType;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(String roleType) {
+		this.roleType = roleType;
+	}
+
+	public List<Reimbursement> getReimbursements() {
+		return reimbursements;
+	}
+
+	public void setReimbursements(List<Reimbursement> reimbursements) {
+		this.reimbursements = reimbursements;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + roleId;
+		result = prime * result + ((roleType == null) ? 0 : roleType.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		if (roleType == null) {
+			if (other.roleType != null)
+				return false;
+		} else if (!roleType.equals(other.roleType))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", email=" + email + ", roleId=" + roleId + ", roleType="
+				+ roleType + "]";
 	}
 
 }
