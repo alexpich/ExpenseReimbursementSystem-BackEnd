d9a87e4f63bb50bb10c098f15df383e6f162eea9
alexpich <pich.alex01@gmail.com>
2018-07-09 11:09:38 -0400

    add annotations to User model

--- src/main/java/com/alexpich/models/User.java
+++ src/main/java/com/alexpich/models/User.java
@@ -2,8 +2,15 @@ package com.alexpich.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Component;
 
@@ -14,4 +21,24 @@ public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@Column(name = "userId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+
+	@Column(name = "roleId")
+	@NotNull
+	@Min(1)
+	@Max(2)
+	private String username;
+
+	private String password;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private int roldId;
 }
