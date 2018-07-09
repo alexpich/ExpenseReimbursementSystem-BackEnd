14f98d7e20bfcba564dbbcd5935d607b1b670210
alexpich <pich.alex01@gmail.com>
2018-07-09 11:17:19 -0400

    add User model

--- src/main/java/com/alexpich/models/User.java
+++ src/main/java/com/alexpich/models/User.java
@@ -22,23 +22,64 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name = "userId")
+	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long userId;
 
-	@Column(name = "roleId")
+	@Column(name = "username")
+	@NotNull
+	private String username;
+
+	@Column(name = "password")
+	@NotNull
+	private String password;
+
+	@Column(name = "firstname")
+	@NotNull
+	private String firstName;
+
+	@Column(name = "lastname")
+	@NotNull
+	private String lastName;
+
+	@Column(name = "email")
+	@NotNull
+	private String email;
+
+	@Column(name = "role_id")
 	@NotNull
 	@Min(1)
 	@Max(2)
-	private String username;
+	private int roleId;
 
-	private String password;
+	@Column(name = "role_type")
+	@NotNull
+	private String roleType;
 
-	private String firstName;
+	public User(@NotNull String username, @NotNull String password, @NotNull String firstName, @NotNull String lastName,
+			@NotNull String email, @NotNull @Min(1) @Max(2) int roleId, @NotNull String roleType) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.roleId = roleId;
+		this.roleType = roleType;
+	}
 
-	private String lastName;
+	public User(Long userId, @NotNull String username, @NotNull String password, @NotNull String firstName,
+			@NotNull String lastName, @NotNull String email, @NotNull @Min(1) @Max(2) int roleId,
+			@NotNull String roleType) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.roleId = roleId;
+		this.roleType = roleType;
+	}
 
-	private String email;
-
-	private int roldId;
 }
